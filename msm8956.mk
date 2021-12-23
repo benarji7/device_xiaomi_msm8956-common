0364501b740d921eaff595e3132a014807605a2e
Benarji Anand <benarji385@gmail.com>
2021-12-23 13:14:52 +0800

    biometrics: Update fingerprint HAL for Pie

--- msm8956.mk
+++ msm8956.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-custom
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8956
 
 # FM
 PRODUCT_PACKAGES += \
